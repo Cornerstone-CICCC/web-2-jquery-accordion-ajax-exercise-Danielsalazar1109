57d2250969a3734e1fd782b203f79dcc59e8eda3
Daniel Salazar <danielsalazar1109@gmail.com>
2024-07-31 16:55:31 -0700

    exercise done

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 $(function() {
-  // your code here
+  $(document).ready(function() {
+    $('.accordion-content').hide();
+    $('.accordion-header:first').addClass('active').next().show();
+  
+    $('.accordion-header').click(function() {
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active').next().slideUp();
+      } else {
+        $('.accordion-header').removeClass('active').next().slideUp();
+        $(this).addClass('active').next().slideDown();
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    $('#load-todos').click(function() {
+      $.ajax({
+        url: 'https://dummyjson.com/todos',
+        method: 'GET',
+        success: function(response) {
+          $('#todo-list').empty();
+          response.todos.forEach(function(todo) {
+            $('#todo-list').append('<li>' + todo.todo + '</li>');
+          });
+        },
+        error: function() {
+          alert('Failed to load to-do items.');
+        }
+      });
+    });
+  });
 })
